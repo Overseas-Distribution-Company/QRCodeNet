a00d62378ab30c51df7afc758a1d3c5e9fe77969
RobbeHeirman <robbeheirman@msn.com>
2020-01-07 10:43:03 +0100

    Simple class extention to generate QR Codes

---
+++ QRCodeScanner/Class1.cs
@@ -0,0 +1,30 @@
+﻿using ZXing;
+using ZXing.QrCode;
+namespace QRCodeScanner
+{
+    public class QrCodeScanner
+    {
+
+        void CreateQrCode(string content, string path, int width, int height)
+        {
+            var option = new QrCodeEncodingOptions()
+            {
+                //DisableECI = true,
+                CharacterSet = "UTF-8",
+                Width = 250,
+                Height = 250
+            };
+            
+            var qr = new BarcodeWriter {Options = option, Format = BarcodeFormat.QR_CODE};
+            var img =  qr.Write(content);
+            img.Save(path);
+            
+        }
+
+        static void Main(string[] args)
+        {
+            var scn = new QrCodeScanner();
+            scn.CreateQrCode("www.github.com,","here.png",100, 100);
+        }
+    }
+}
